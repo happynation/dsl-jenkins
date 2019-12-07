7cb601a56c81623f5e8c2f7fba8b107242d8fc50
vovatran1993 <vovatran1993@gmail.com>
2019-12-07 15:49:02 -0600

    if added

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -12,11 +12,17 @@ pipeline {
         stage("Download Terraform"){
             steps{
                 ws("tmp/"){
-                    sh "pwd"
-                    sh "wget https://releases.hashicorp.com/terraform/0.12.17/terraform_0.12.17_linux_amd64.zip"
-                    sh "unzip terraform_0.12.17_linux_amd64.zip"
-                    sh "sudo mv terraform /bin"
-                    sh "terraform version"
+                    script {
+                        def exists = fileExists 'terraform_0.12.17_linux_amd64.zip'
+                        if (exists) {
+                            sh "unzip -o terraform_0.12.17_linux_amd64.zip"
+                            sh "sudo mv -f terraform /bin"
+                            sh "terraform version"
+                        } else {
+                            sh "wget https://releases.hashicorp.com/terraform/0.12.7/terraform_0.12.17_linux_amd64.zip"
+                            sh "unzip -o terraform_0.12.17_linux_amd64.zip"
+                            sh "sudo mv -f terraform /bin"
+                            sh "terraform version"
                 }
             }
         }
