d033990a3623026699cb22eaab468d07887ce4be
vovatran1993 <vovatran1993@gmail.com>
2019-12-07 15:58:00 -0600

    packer added

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -28,5 +28,24 @@ pipeline {
                 }
             }
         }
+        stage("Download Packer"){
+            steps{
+                ws("tmp/"){
+                    script {
+                        def exists = fileExists 'packer_1.4.5_linux_amd64.zip'
+                        if (exists) {
+                            sh "unzip -o packer_1.4.5_linux_amd64.zip"
+                            sh "sudo mv -f packer /bin"
+                            sh "terraform version"
+                        } else {
+                            sh "wget https://releases.hashicorp.com/packer/1.4.5/packer_1.4.5_linux_amd64.zip"
+                            sh "unzip -o packer_1.4.5_linux_amd64.zip"
+                            sh "sudo mv -f packer /bin"
+                            sh "packer version"
+                        }
+                    }
+                }
+            }
+        }
     }
 }
