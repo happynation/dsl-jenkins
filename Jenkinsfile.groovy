b417b87ce5418f697fc8ded8687923b02a2395c0
vovatran1993 <vovatran1993@gmail.com>
2019-12-07 15:37:00 -0600

    version

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -15,6 +15,8 @@ pipeline {
                     sh "pwd"
                     sh "wget https://releases.hashicorp.com/terraform/0.12.17/terraform_0.12.17_linux_amd64.zip"
                     sh "unzip terraform_0.12.17_linux_amd64.zip"
+                    sh "sudo mv terraform /bin"
+                    sh "terraform version"
                 }
             }
         }
